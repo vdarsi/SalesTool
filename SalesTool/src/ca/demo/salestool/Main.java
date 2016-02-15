dd248dc1480d5f27273d8e2fc88fab343e5ac1e4
Vijay Darsi <vbdarsi@gmail.com>
2016-02-15 22:23:19 +0530

    Initial checkin of sales tool

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,19 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+   private static void displayGreeting() {
+		
+		System.out.println("HelloHappy sales people");
+		System.out.println("This app shows sales data");
+	}
+
+}
---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,15 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	
+	int data[] = {0,4,40};
+	
+	
+	public void display() {
+		
+		System.out.println("TODO implemet data display");
+	}
+	
+	
+
+}
